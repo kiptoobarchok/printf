a5fce2caf7cd92950bee1f5b3be8264daafecedd
Gideon5348 <gideonmutai096@gmail.com>
2023-06-27 15:55:06 -0700

    printf

--- char.c
+++ char.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_char - Prints a character.
+ * @args: A va_list containing the argument to print
+ *
+ * Return: The number of characters printed.
+ */
+
 int print_char(va_list args)
 {
 	char c;
