926ad14cc5e34595f8036ecd510a4335a4798c1f
Gideon5348 <gideonmutai096@gmail.com>
2023-06-27 16:05:22 -0700

    string update

--- string.c
+++ string.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * print_str - Prints a string.
+ * @args: A va_list containing the string to be printed.
+ *
+ * Return: The number of characters printed
+ */
+
 int print_str(va_list args)
 {
 	char *str;
