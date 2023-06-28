87f22579a6e18da0f04ab72550edd8457c89659d
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-06-28 08:24:29 -0700

    number test file

---
+++ test_file/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "../main.h"
+
+/**
+ * main - The main entry
+ * Return: 0 always.
+ */
+int main(void)
+{
+    int len, len2;
+    int num = 998;
+
+    len = _printf("Length:[%d, %i]\n", num, num);
+    len2 = printf("Length:[%d, %i]\n", num, num);
+    printf("len: %d,  len2: %d\n", len, len2);
+
+    return (0);
+}
