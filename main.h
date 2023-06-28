d92a489b5820619b57a24aadef16ee1497d646a2
Gideon5348 <gideonmutai096@gmail.com>
2023-06-28 06:52:57 -0700

    main.h

--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-int _printf(const char* format, ...);
+int _printf(const char *format, ...);
 
 int _putchar(char c);
 int _puts(const char *str);
@@ -13,11 +13,19 @@ int print_char(va_list);
 int print_str(va_list);
 int print_pcnt(va_list);
 
+
+/**
+ * struct printer - template to define format specifier.
+ * @c: format specifier.
+ * @f: pointer to a function to print format specifier.
+ *
+ */
+
 typedef struct printer
 {
 	char c;
 	int (*f)(va_list args);
-}printer;
+} printer;
 
 int (*fmt(char c))(va_list args);
 
