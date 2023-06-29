119ca5e4bda0cbcb915affa615d0d0780d19af22
Gideon5348 <gideonmutai096@gmail.com>
2023-06-29 10:32:00 -0700

    print %d update

--- print_d.c
+++ print_d.c
@@ -1,20 +1,35 @@
 #include "main.h"
 
+/**
+ * print_d - prints integer and count number of digits.
+ * @args: variable argument list.
+ *
+ * Return: value of x.
+ */
+
 int print_d(va_list args)
 {
-       	int a = va_arg(args, int);
-       	unsigned int n = (a < 0) ? -a : a;
- 	unsigned int x = 1;
+	int a = va_arg(args, int);
+	unsigned int n = (a < 0) ? -a : a;
+	unsigned int x = 1;
 
 	while (n / 10)
- 	{
+	{
 		n = n / 10;
 		x++;
 	}
-	
+
 	digit_print(a, 0);
-	return x;
+	return (x);
 }
+
+/**
+ * digit_print - recursive funcion to print integer digits.
+ * @c: integer.
+ * @count: current count of digits.
+ *
+ * Return: count.
+ */
 
 int digit_print(int c, int count)
 {
@@ -30,8 +45,8 @@ int digit_print(int c, int count)
 	}
 
 	_putchar((c % 10) + '0');
- 	count++;
+	count++;
 
-	return count;
+	return (count);
 }
 
