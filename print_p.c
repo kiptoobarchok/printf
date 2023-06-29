703ffcec8623a0c180328bb32db4df042b6122ef
Gideon5348 <gideonmutai096@gmail.com>
2023-06-29 16:59:55 -0700

    prin tp update

--- print_p.c
+++ print_p.c
@@ -10,34 +10,21 @@
 
 int print_p(va_list args)
 {
-	void *ptr = va_arg(args, void *);
-
-	unsigned long int address = (unsigned long int)ptr;
+	void *ptr;
+	unsigned long int address;
 	int count = 0;
 
-	count += _putchar('0');
-	count += _putchar('x');
-	count += print_hex(address);
+	ptr = va_arg(args, void *);
 
-	return (count);
-}
+	if (!ptr)
+	{
+		/* No need for nil_string variable */
+		return (_puts("(nil)"));
+	}
 
-/**
- * print_hex - Prints a hexadecimal number
- * @n: The number to be printed
- *
- * Return: The number of characters printed
- */
-
-int print_hex(unsigned long int n)
-{
-	char hex_digits[] = "0123456789abcdef";
-	int count = 0;
-
-	if (n / 16)
-		count += print_hex(n / 16);
-
-	count += _putchar(hex_digits[n % 16]);
+	address = *((unsigned long int *)ptr);
+	count += _putchar("0x");
+	print_hex(address);
 
 	return (count);
 }
