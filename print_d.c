4ac35aa5ae4efd10cda7adabcc107576433c8d06
kiptoobarchok <calebkiptoo9090@gmail.com>
2023-06-29 11:41:54 -0700

    updated task 1

--- print_d.c
+++ print_d.c
@@ -5,26 +5,23 @@ int print_d(va_list args)
 	int n = va_arg(args, int);
     int div;
     int count;
-    unsigned int num;
    
     div = 1;
     count = 0;
 
     if (n < 0)
     {
-        count += ('-');
-        num = n * -1;
+        count += _putchar('-');
+        n *= -1;
     }
-    else
-        num = n;
 
-    for (; num / div > 9; )
+    for (; n / div > 9; )
         div *= 10;
 
     for (; div != 0; )
     {
-        count += _putchar('0' + num / div);
-        num %= div;
+        count += _putchar('0' + n / div);
+        n %= div;
         div /= 10;
     }
 
