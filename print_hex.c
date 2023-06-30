78dd816d722fca66c95c16377b7b3c88dff5ae71
Gideon5348 <gideonmutai096@gmail.com>
2023-06-30 15:10:30 -0700

    print_hex.c file update

--- print_hex.c
+++ print_hex.c
@@ -2,43 +2,51 @@
 
 /**
  * print_hex - Prints an unsigned integer in Hex
- * @n: Integer to print.
- * @base: number used for representation.
+ * @args: va_list arguments.
  *
- * Return: Number of characters printed
+ * Return: counter.
  */
 
-int print_hex(va_list args) {
-    int i;
-    int *array;
-    int counter = 0;
-    unsigned int num = va_arg(args, unsigned int);
-    unsigned int temp = num;
+int print_hex(va_list args)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(args, unsigned int);
+	unsigned int temp = num;
 
-    if (num == 0) {
-        _putchar('0');
-        return 1;
-    }
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
 
-    while (temp != 0) {
-        temp /= 16;
-        counter++;
-    }
+	while (temp != 0)
+	{
+		temp /= 16;
+		counter++;
+	}
 
-    array = malloc(counter * sizeof(int));
+	array = malloc(counter * sizeof(int));
 
-    for (i = 0; i < counter; i++) {
-        array[i] = num % 16;
-        num /= 16;
-    }
-    for (i = counter - 1; i >= 0; i--) {
-        if (array[i] <= 9) {
-            _putchar(array[i] + '0');
-        } else {
-            _putchar(array[i] - 10 + 'A');
-        }
-    }
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = num % 16;
+		num /= 16;
+	}
 
-    free(array);
-    return counter;
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] <= 9)
+		{
+			_putchar(array[i] + '0');
+		}
+		else
+		{
+			_putchar(array[i] - 10 + 'A');
+		}
+	}
+
+	free(array);
+	return (counter);
 }
